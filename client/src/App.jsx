3fab4317cb640fa595b4350d1edccf906ff0c500
devonbarangan <barangandevon@gmail.com>
2026-02-27 14:30:53 +0800

    chore: added route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import LandingPage from "./pages/LandingPage";
+import Skills from "./pages/Skills";
+import Experience from "./pages/Experience";
+import Contacts from "./pages/Contacts";
+import Projects from "./pages/Projects";
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/skills" element={<Skills />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="*" element={<div>404 Not Found</div>} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
