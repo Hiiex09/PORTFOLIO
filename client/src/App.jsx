717a1b3b94e3553a4ae22ceb62d55484e6087b83
barangandevon <barangandevon@gmail.com>
2026-03-14 22:48:56 +0800

    chore: modified portfolio

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,23 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import LandingPage from "./pages/LandingPage";
-import Skills from "./pages/Skills";
-import Experience from "./pages/Experience";
-import Contacts from "./pages/Contacts";
-import Projects from "./pages/Projects";
-const App = () => {
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Contact from "./components/Contact";
+
+function App() {
   return (
-    <BrowserRouter>
+    <div className="bg-base-100 text-base-content">
       <Navbar />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/skills" element={<Skills />} />
-        <Route path="/experience" element={<Experience />} />
-        <Route path="/contacts" element={<Contacts />} />
-        <Route path="*" element={<div>404 Not Found</div>} />
-      </Routes>
-    </BrowserRouter>
+
+      <Hero />
+
+      <Projects />
+
+      <Skills />
+
+      <Contact />
+    </div>
   );
-};
+}
 
 export default App;
